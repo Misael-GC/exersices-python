d72c1168a8365f75e3b6ea2120198e9a48900130
Misael-GC <misael.gomez1623@gmail.com>
2022-10-15 09:08:12 -0500

    list cpmprehensions, raiz cuadrada de un rango

--- dictionary_comprehensions.py
+++ dictionary_comprehensions.py
@@ -5,7 +5,7 @@ def run():
     #     if i % 3 != 0:
     #         my_dict[i] = i**3
 
-    my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+    my_dict = {i: i**(0.5) for i in range(1, 1000)}
 
     print(my_dict)
 
