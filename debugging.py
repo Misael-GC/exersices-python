ed89f4d8a9fd537bf0e57d50f8d906e1a3cd08e3
Misael-GC <misael.gomez1623@gmail.com>
2022-10-16 17:57:08 -0500

    manejo de errores try except, raise, evitar números negativos, y letras

--- debugging.py
+++ debugging.py
@@ -1,14 +1,27 @@
+from multiprocessing.sharedctypes import Value
+
+
 def divisors(num):
-    divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0: #si es el resultado es 1
-            divisors.append(i) #lo agregamos
-    return divisors
+    try:
+        if num < 0:
+            raise ValueError('No se puede ingresar un número negativo')
+        divisors = [i for i in range(1, num + 1) if num % i == 0]
+        #   for i in range(1, num + 1):
+        #   if num % i == 0: #si es el resultado es 1
+        #     divisors.append(i) #lo agregamos
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        print('ingresa un número positivo')
+        return False
 
 def run():
-    num = int(input('ingresa un número: ')) #ingrese un numero y convertir a entero
-    print(divisors(num))
-    print('Terminó mi programa')
+    try:    
+        num = int(input('ingresa un número: ')) #ingrese un numero y convertir a entero
+        print(divisors(num))
+        print('Terminó mi programa')
+    except:
+        print('Sintaxis error, debes ingresar un número')
 
 if __name__ == '__main__':
     run()
