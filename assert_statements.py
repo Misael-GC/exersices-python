f2f7439742443193142dbda3037756c3a668515e
Misael-GC <misael.gomez1623@gmail.com>
2022-10-16 20:21:30 -0500

    update

--- assert_statements.py
+++ assert_statements.py
@@ -2,16 +2,23 @@ from multiprocessing.sharedctypes import Value
 
 
 def divisors(num):
+    try:
+        if num < 0 or num == 0:
+            raise ValueError('No se puede ingresar un número negativo')
         divisors = [i for i in range(1, num + 1) if num % i == 0]
         #   for i in range(1, num + 1):
         #   if num % i == 0: #si es el resultado es 1
         #     divisors.append(i) #lo agregamos
         return divisors
+    except ValueError as ve:
+        print(ve)
+        print('ingresa un número positivo')
+        return False
 
 def run():
-        num = int(input('ingresa un número: ')) #ingrese un numero y convertir a entero
-        assert num > 0, 'Debes ingresar un número positivo' #.isnumeric -> metodo dice es un numero?
-        print(divisors(num)) #volvemos a num en un entero
+        num = input('ingresa un número: ') #ingrese un numero y convertir a entero
+        assert num.isnumeric(), 'Debes ingresar un número entero y que no sea negativo' #.isnumeric -> metodo dice es un numero?
+        print(divisors(int(num))) #volvemos a num en un entero
         print('Terminó mi programa')
 
 if __name__ == '__main__':
